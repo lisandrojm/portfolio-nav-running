677a8c398c479e46059762c276b69dee40c7b213
lisandrojm <lisandrojm@gmail.com>
2023-12-28 15:51:12 -0300

    background.module.css

--- src/app/_components/Background.tsx
+++ src/app/_components/Background.tsx
@@ -1,99 +1,80 @@
 /* src/app/_components/Background.tsx */
 
+import styles from '@/_styles/_components/Background.module.css';
 import BgIcons from '@/_components/_icons/BgIcons';
 
 export default function Background() {
   return (
-    <section id="animated-cta-bar">
-      <ul className="floating-icons">
-        <li className="drift drift1 delay1">
-          <div className="float float1">
-            {/*GitHub */}
-            <BgIcons kind="github" size={2} />
+    <section className={`${styles.animated}`}>
+      <ul className={`${styles.floating}`}>
+        <li className={`${styles.drift} ${styles.drift1} ${styles.delay1} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float1}`}>
+            <BgIcons kind="github" size={3} />
           </div>
         </li>
-        <li className="drift drift2 delay3">
-          <div className="float float2">
-            {/*React */}
+        <li className={`${styles.drift} ${styles.drift2} ${styles.delay3} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float2}`}>
+            <BgIcons kind="bootstrap" size={3} />
+          </div>
+        </li>
+        <li className={`${styles.drift} ${styles.drift3} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float3}`}>
+            <BgIcons kind="html" size={3} />
+          </div>
+        </li>
+        <li className={`${styles.drift} ${styles.drift5} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float5}`}>
+            <BgIcons kind="tailwind" size={3} />
+          </div>
+        </li>
+        <li className={`${styles.drift} ${styles.drift6} ${styles.delay2} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float6}`}>
+            <BgIcons kind="nextjs" size={3} />
+          </div>
+        </li>
+        <li className={`${styles.drift} ${styles.drift7} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float7}`}>
+            <BgIcons kind="vercel" size={3} />
+          </div>
+        </li>
+        <li className={`${styles.drift} ${styles.drift8} ${styles.delay5} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float8}`}>
+            <BgIcons kind="js" size={3} />
+          </div>
+        </li>
+        <li className={`${styles.drift} ${styles.drift9} ${styles.delay5} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float9}`}>
             <BgIcons kind="react" size={3} />
           </div>
         </li>
-        <li className="drift drift3">
-          <div className="float float3">
-            {/*Html */}
-            <BgIcons kind="html" size={2} />
+        <li className={`${styles.drift} ${styles.drift10} ${styles.delay3} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float10}`}>
+            <BgIcons kind="nodejs" size={3} />
           </div>
         </li>
-        <li className="drift drift4 delay2">
-          <div className="float float4">
-            {/*Nextjs */}
-            <BgIcons kind="nextjs" size={3} />
-          </div>
-        </li>
-        <li className="drift drift5">
-          <div className="float float5">
-            {/*Tailwind */}
-            <BgIcons kind="tailwind" size={2} />
-          </div>
-        </li>
-        <li className="drift drift6 delay2">
-          <div className="float float6">
-            {/*Nextjs */}
-            <BgIcons kind="nextjs" size={3} />
-          </div>
-        </li>
-        <li className="drift drift7">
-          <div className="float float7">
-            {/*Vercel */}
-            <BgIcons kind="vercel" size={2} />
-          </div>
-        </li>
-        <li className="drift drift8 delay5">
-          <div className="float float8">
-            {/*Js */}
-            <BgIcons kind="js" size={1} />
-          </div>
-        </li>
-        <li className="drift drift9 delay5">
-          <div className="float float9">
-            {/*Boostrap*/}
-            <BgIcons kind="bootstrap" size={2} />
-          </div>
-        </li>
-        <li className="drift drift10 delay3">
-          <div className="float float10">
-            {/*Nodejs*/}
-            <BgIcons kind="nodejs" size={1} />
-          </div>
-        </li>
-        <li className="drift drift5 delay4">
-          <div className="float float7">
-            {/*Express*/}
+        <li className={`${styles.drift} ${styles.drift5} ${styles.delay4} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float7}`}>
             <BgIcons kind="express" size={3} />
           </div>
         </li>
-        <li className="drift drift2 delay5">
-          <div className="float float3">
-            {/*Nestjs*/}
-            <BgIcons kind="nestjs" size={2} />
+        <li className={`${styles.drift} ${styles.drift2} ${styles.delay5} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float3}`}>
+            <BgIcons kind="nestjs" size={3} />
           </div>
         </li>
-        <li className="drift drift3 delay1 mt-5">
-          <div className="float float8">
-            {/*MongoDb*/}
-            <BgIcons kind="mongodb" size={2} />
+        <li className={`${styles.drift} ${styles.drift3} ${styles.delay1} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float8}`}>
+            <BgIcons kind="mongodb" size={3} />
           </div>
         </li>
-        <li className="drift drift6 delay5">
-          <div className="float float1">
-            {/*Typescript*/}
+        <li className={`${styles.drift} ${styles.drift6} ${styles.delay5} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float1}`}>
             <BgIcons kind="ts" size={3} />
           </div>
         </li>
-        <li className="drift drift9 delay2">
-          <div className="float float10">
-            {/*Git*/}
-            <BgIcons kind="git" size={1} />
+        <li className={`${styles.drift} ${styles.drift9} ${styles.delay2} ${styles.floating_li}`}>
+          <div className={`${styles.float} ${styles.float10}`}>
+            <BgIcons kind="git" size={3} />
           </div>
         </li>
       </ul>

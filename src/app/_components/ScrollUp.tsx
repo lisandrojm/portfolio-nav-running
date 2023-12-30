74d3cc4b92f419d5fe484ff1bde3ec6fac97e477
lisandrojm <lisandrojm@gmail.com>
2023-12-30 06:37:32 -0300

    refactoring

--- src/app/_components/ScrollUp.tsx
+++ src/app/_components/ScrollUp.tsx
@@ -23,7 +23,7 @@ const ScrollUp: React.FC = () => {
   };
 
   return (
-    <div className={`fixed h-10 w-10 pb-1 rounded-full flex justify-center items-center bottom-6 md:bottom-10 right-0 cursor-pointer transition-all ease-out duration-500 z-50 border-solid border border-orange  ${showButton ? 'right-6  md:right-10' : 'translate-x-full '}`} onClick={scrollToTop}>
+    <div className={`fixed h-10 w-10 pb-1 rounded-full flex justify-center items-center bottom-10 md:bottom-10 right-0 cursor-pointer transition-all ease-out duration-500 z-50 border-solid border border-orange  ${showButton ? 'right-6  md:right-10' : 'translate-x-full '}`} onClick={scrollToTop}>
       <h1 className="text-lg">↑</h1>
     </div>
   );
